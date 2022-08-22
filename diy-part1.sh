e9e650bedbd82535a84887e156f583ce1d545f91
HaoCheng <ch1994@outlook.com>
2022-08-22 12:41:19 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,11 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
 echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 git clone https://github.com/mchome/openwrt-vlmcsd package/openwrt-vlmcsd
 git clone https://github.com/mchome/luci-app-vlmcsd package/luci-app-vlmcsd
